c4dfe9b72b7a8e80c8d3ba3fdfbcdb59263c4a8c
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2026-05-31 19:04:19 +0300

    Move help to modal and refine interactions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='reflection-pwa-v5';
+const CACHE='reflection-pwa-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
