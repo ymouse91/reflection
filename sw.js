7e71d6a79ee288cacedbc792e231e1f1edf7a395
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2026-05-31 16:10:10 +0300

    Initial Reflection PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='reflection-pwa-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
